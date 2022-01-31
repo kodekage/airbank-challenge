c63197b88e2bac66a27d270da083605c8f2ae7b3
Prosper Opara <oparaprosper79@gmail.com>
2022-01-31 18:54:02 +0100

    chore: setup backend service

---
+++ backend/src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { PrismaService } from './prisma.service'
+import { join } from 'path'
+
+@Module({
+  imports: [
+    GraphQLModule.forRoot({
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      buildSchemaOptions: { dateScalarMode: 'timestamp' },
+    }),
+  ],
+  controllers: [],
+  providers: [PrismaService],
+})
+export class AppModule {}
