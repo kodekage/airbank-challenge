79126bc95f58baafed17a6916b8767b662cd32bb
Prosper Opara <oparaprosper79@gmail.com>
2022-02-01 13:23:34 +0100

    chore: fix lint and add action

--- backend/src/modules/transactions/transactions.module.ts
+++ backend/src/modules/transactions/transactions.module.ts
@@ -1,9 +1,8 @@
-import { Module } from "@nestjs/common";
-import { PrismaService } from "../../prisma.service";
-import { TransactionResolver } from "./transactions.resolver";
+import { Module } from '@nestjs/common';
+import { PrismaService } from '../../prisma.service';
+import { TransactionResolver } from './transactions.resolver';
 
 @Module({
-    providers: [TransactionResolver, PrismaService],
-  })
-  export class TransactionsModule {}
-  
+  providers: [TransactionResolver, PrismaService],
+})
+export class TransactionsModule {}
